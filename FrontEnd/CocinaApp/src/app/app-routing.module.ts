6b1729ae13929f2e53b113432ee2d46862c8502a
pegasojuegos <mhp74185@gmail.com>
2024-05-21 19:20:29 +0200

    Añadida página listado de recetas por categoria con sus estilos, mejorado header y footer

--- FrontEnd/CocinaApp/src/app/app-routing.module.ts
+++ FrontEnd/CocinaApp/src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { RecetaDetalleComponent } from './receta-detalle/receta-detalle.component';
 import { RecetasComponent } from './recetas/recetas.component';
 import { InicioComponent } from './inicio/inicio.component';
+import { CategoriaRecetasComponent } from './categoria-recetas/categoria-recetas.component';
 
 const routes: Routes = [
   {path: 'recetas', component: RecetasComponent },
   {path: 'recetas/:id', component: RecetaDetalleComponent },
   {path : "inicio",component:InicioComponent},
-  {path : '', redirectTo:"inicio",pathMatch: 'full'}
+  {path : '', redirectTo:"inicio",pathMatch: 'full'},
+  {path : 'categoria/:id', component:CategoriaRecetasComponent}
 ];
 
 @NgModule({
