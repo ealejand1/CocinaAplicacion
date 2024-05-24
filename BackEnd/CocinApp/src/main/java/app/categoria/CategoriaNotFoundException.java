f57f3b65f1abc8b27fdc7fe8913d7f31ea9d4ea1
ekisam <104451325+ealejand1@users.noreply.github.com>
2024-05-24 18:44:07 +0200

    Eliminando Conflictos

--- BackEnd/CocinApp/src/main/java/app/categoria/CategoriaNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package app.categoria;
-
-public class CategoriaNotFoundException extends RuntimeException{
-
-	public CategoriaNotFoundException(Long id) {
-		super("UnU ~ No se ha podido encontrar la categoria con ID:"+id);
-	}
-}
--- BackEnd/CocinApp/src/main/java/app/receta/RecetaNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package app.receta;
-
-public class RecetaNotFoundException extends RuntimeException{
-
-	public RecetaNotFoundException(Long id) {
-		super("UnU ~ No se ha podido encontrar la receta con ID:"+id);
-	}
-}
