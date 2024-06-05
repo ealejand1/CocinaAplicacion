64e3049bfcb909eba566599ce5ed24ee5a65501a
ekisam <104451325+ealejand1@users.noreply.github.com>
2024-06-05 02:27:40 +0200

    cambios3Prod

--- FrontEnd/CocinaApp/src/environments/environment.prod.ts
+++ FrontEnd/CocinaApp/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    urlHost: 'http://34.201.52.222',
-    urlApi: 'http://34.201.52.222/api/v1',
+    urlHost: 'http://100.27.79.255',
+    urlApi: 'http://100.27.79.255/api/v1',
   };
   
